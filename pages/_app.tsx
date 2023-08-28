e1d168c2cb54e7756d386f22d0c546f0cfd6af98
AlexOrekhov <apexof@gmail.com>
2023-08-28 19:48:54 +0300

    add linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,15 @@
 import '../styles/global.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import {
-  RainbowKitProvider,
-  getDefaultWallets,
-  connectorsForWallets,
-} from '@rainbow-me/rainbowkit';
-import {
-  argentWallet,
-  trustWallet,
-  ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
+import { RainbowKitProvider, getDefaultWallets, connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { argentWallet, trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  polygonMumbai,
-} from 'wagmi/chains';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    polygonMumbai
-  ],
-  [publicProvider()]
-);
+const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, optimism, arbitrum, polygonMumbai], [publicProvider()]);
 
-const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string
-
+const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string;
 
 const { wallets } = getDefaultWallets({
   appName: 'RainbowKit demo',
@@ -49,11 +25,7 @@ const connectors = connectorsForWallets([
   ...wallets,
   {
     groupName: 'Other',
-    wallets: [
-      argentWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-    ],
+    wallets: [argentWallet({ projectId, chains }), trustWallet({ projectId, chains }), ledgerWallet({ projectId, chains })],
   },
 ]);
 
