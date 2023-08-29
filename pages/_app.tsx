304ed81a4135be20b1bc2fc8b2398ac660f3cdc9
AlexOrekhov <apexof@gmail.com>
2023-08-29 09:31:35 +0300

    refactoring wagmi config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,12 @@ import '../styles/global.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { RainbowKitProvider, getDefaultWallets, connectorsForWallets } from '@rainbow-me/rainbowkit';
-import { argentWallet, trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import React from 'react';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, optimism, arbitrum, polygonMumbai], [publicProvider()]);
+const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, polygonMumbai], [publicProvider()]);
 
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string;
 
@@ -18,17 +17,7 @@ const { wallets } = getDefaultWallets({
   chains,
 });
 
-const demoAppInfo = {
-  appName: 'Rainbowkit Demo',
-};
-
-const connectors = connectorsForWallets([
-  ...wallets,
-  {
-    groupName: 'Other',
-    wallets: [argentWallet({ projectId, chains }), trustWallet({ projectId, chains }), ledgerWallet({ projectId, chains })],
-  },
-]);
+const connectors = connectorsForWallets(wallets);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -40,7 +29,7 @@ const wagmiConfig = createConfig({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+      <RainbowKitProvider chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
