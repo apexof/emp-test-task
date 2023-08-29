74d37cd22ebc71b9c100e051e9efd79194a1c0e1
AlexOrekhov <apexof@gmail.com>
2023-08-29 09:32:50 +0300

    add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,57 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import React from 'react';
+import Layout from '../components/Layout';
+import { Button, DatePicker, Form, Input } from 'antd';
+
+type FieldType = {
+  cutoffDate?: number;
+  decisionDate?: number;
+  decisionProvider?: string;
+  description?: string;
+};
 
 const Home: NextPage = () => {
+  const onFinish = (values: FieldType) => {
+    console.log('Success:', values.cutoffDate);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
-    </div>
+    <Layout>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType> label="Cutoff Date" name="cutoffDate" rules={[{ required: true }]}>
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item<FieldType> label="Decision Date" name="decisionDate" rules={[{ required: true }]}>
+          <DatePicker />
+        </Form.Item>
+        <Form.Item<FieldType> label="Decision Provider" name="decisionProvider" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType> label="Description" name="description" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Layout>
   );
 };
 
