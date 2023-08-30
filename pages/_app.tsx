5cc86ad74fc5dea753a69bcede7f8c159e47d714
AlexOrekhov <apexof@gmail.com>
2023-08-30 15:56:31 +0300

    add rpc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import '../styles/global.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { RainbowKitProvider, getDefaultWallets, connectorsForWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
+import { Chain, configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai as polygonMumbaiDefault } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import React from 'react';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, polygonMumbai], [publicProvider()]);
+export const polygonMumbai = {
+  ...polygonMumbaiDefault,
+  rpcUrls: {
+    public: { http: ['https://polygon-mumbai-bor.publicnode.com'] },
+    default: { http: ['https://polygon-mumbai-bor.publicnode.com'] },
+  },
+} as const satisfies Chain;
+
+const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, optimism, polygon, polygonMumbai], [publicProvider()]);
 
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string;
 
