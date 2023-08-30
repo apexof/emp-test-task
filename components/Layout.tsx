949500dcf68f1de554ed6395ec6c160a9b30e901
AlexOrekhov <apexof@gmail.com>
2023-08-30 19:35:22 +0300

    add navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
@@ -8,7 +9,11 @@ export default function Layout({ children }) {
       <Head>
         <meta name="description" content="EMP" />
       </Head>
-      <header style={{ padding: 10 }}>
+      <header style={{ padding: 10, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <div>
+          <Link href="/">Create Market</Link>&nbsp;
+          <Link href="/markets">All Markets</Link>
+        </div>
         <ConnectButton />
       </header>
       <main>{children}</main>
