84bde1021335aafe32681188eb4571a459b3b98b
AlexOrekhov <apexof@gmail.com>
2023-08-30 15:56:19 +0300

    add constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,4 @@
 export const EMP_API_URL = 'https://emp-backend-test.fly.dev';
 export const PREDICTION_MARKET_FACTORY_ADDRESS = '0x595a74dde1b1d08a48943a81602bc334474ce487';
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const DATE_FORMAT = 'YYYY-MM-DD hh:mm:ss';
