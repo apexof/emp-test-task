028e13653d6cb02bbd99facefa2e3e7c5ec0a4af
AlexOrekhov <apexof@gmail.com>
2023-08-30 20:47:28 +0300

    fix dates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Button, DatePicker, Form, Input } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 import type { NextPage } from 'next';
 import React, { useState } from 'react';
-import { usePublicClient } from 'wagmi';
+import { useContractRead, usePublicClient } from 'wagmi';
 import Layout from '../components/Layout';
 import { ModalCreateMarket } from '../components/ModalCreateMarket';
 import { ModalExistMarketAlert } from '../components/ModalExistMarketAlert';
@@ -24,48 +24,31 @@ const Home: NextPage = () => {
   const [existMarketAddress, setExistMarketAddress] = useState<`0x${string}`>();
 
   const onFinish = async (values: CreateMarketForm) => {
-    // values.cutoffDate.millisecond(0);
-    // console.log('1692576000000', 1692576000000);
-    // console.log('values.cutoffDate.valueOf()', values.cutoffDate.valueOf());
-    console.log('1692576000000', dayjs(1692576000000).format('DD/MM/YYYY, hh:mm:ss:SSS'));
-    console.log('picker', values.cutoffDate.valueOf(), dayjs(values.cutoffDate).format('DD/MM/YYYY, hh:mm:ss:SSS'));
-    // console.log('', values.cutoffDate.millisecond(0).format('DD/MM/YYYY, hh:mm:ss:SSS'));
+    const cutoffDate = values.cutoffDate.millisecond(0);
+    const decisionDate = values.decisionDate.millisecond(0);
+
     const marketAddress = await publicClient.readContract<typeof predictionMarketFactory.abi, string>({
       address: predictionMarketFactory.address,
       abi: predictionMarketFactory.abi,
       functionName: 'getMarket',
-      args: [values.description, BigInt(values.cutoffDate.valueOf())],
+      args: [values.description, BigInt(cutoffDate.valueOf())],
     });
     if (marketAddress !== ZERO_ADDRESS) {
-      console.log('Market already exists');
       if (marketAddress) {
         setExistMarketAddress(marketAddress);
         setIsExistMarketModalOpen(true);
       }
     } else {
-      setForm(values);
+      setForm({ ...values, decisionDate, cutoffDate });
       setIsCreateMarketModalOpen(true);
     }
   };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
-
   return (
     <Layout>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
+      <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} style={{ maxWidth: 600 }} initialValues={{ remember: true }} onFinish={onFinish} autoComplete="off">
         <Form.Item<CreateMarketForm> label="Cutoff Date" name="cutoffDate" rules={[{ required: true }]}>
-          <DatePicker />
+          <DatePicker showTime />
         </Form.Item>
 
         <Form.Item<CreateMarketForm> label="Decision Date" name="decisionDate" rules={[{ required: true }]}>
