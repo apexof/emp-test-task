242f2ffefe39b16b4b241d05042b8063d3619207
AlexOrekhov <apexof@gmail.com>
2023-08-31 19:55:33 +0300

    add state

--- pages/markets/[address].tsx
+++ pages/markets/[address].tsx
@@ -5,6 +5,19 @@ import { useContractReads } from 'wagmi';
 import Layout from '../../components/Layout';
 import { DATE_FORMAT } from '../../constants';
 import { predictionMarket } from '../../constants/abi/predictionMarket';
+import { useRouter } from 'next/router';
+import { ContractFunctionConfig } from 'viem';
+
+const functionNames = ['getDescription', 'getCutoffDate', 'getState'];
+
+const getContracts = (marketAddress: `0x${string}`): ContractFunctionConfig[] => {
+  const contracts = functionNames.map(functionName => ({
+    abi: predictionMarket.abi,
+    address: marketAddress,
+    functionName,
+  }));
+  return contracts;
+};
 
 interface Props {
   marketAddress: `0x${string}`;
@@ -12,19 +25,7 @@ interface Props {
 
 export const Market: FC<Props> = props => {
   const { marketAddress } = props;
-
-  const contracts = [
-    {
-      abi: predictionMarket.abi,
-      address: marketAddress,
-      functionName: 'getDescription',
-    },
-    {
-      abi: predictionMarket.abi,
-      address: marketAddress,
-      functionName: 'getCutoffDate',
-    },
-  ];
+  const contracts = getContracts(marketAddress);
 
   const { data, error, isLoading } = useContractReads<any[], boolean, any>({ contracts });
 
@@ -40,6 +41,7 @@ export const Market: FC<Props> = props => {
           <div>Description: {data[0].result}</div>
           <div>Cutoff Date: {dayjs(Number(data[1].result)).format(DATE_FORMAT)}</div>
           <div>Cutoff Timestamp: {Number(data[1].result)}</div>
+          <div>State: {Number(data[2].result)}</div>
         </div>
       ) : null}
     </Layout>
@@ -51,7 +53,7 @@ export default Market;
 export async function getStaticProps({ params }) {
   return {
     props: {
-      marketAddress: params.id,
+      marketAddress: params.address,
     },
   };
 }
